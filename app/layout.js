b71837fea2d3c6762042897d79ec35a5ead22dfc
Don Barto Jr <donbartojunior@gmail.com>
2026-08-14 15:37:37 -0400

    add verce speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: 'I Saw U',
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-screen bg-slate-950 text-slate-100">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
